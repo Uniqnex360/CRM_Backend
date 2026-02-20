1e14e66ef0483cf74484216a3735f75f564df7aa
R <sangamithra@uniqnex360.com>
2026-02-20 18:00:31 +0530

    LEADS

--- database.py
+++ database.py
@@ -9,7 +9,16 @@ if not DATABASE_URL:
     raise Exception("DATABASE_URL not found in .env")
 
 
-database: AsyncIOMotorDatabase = AsyncIOMotorClient(DATABASE_URL).crm_db
+# database: AsyncIOMotorDatabase = AsyncIOMotorClient(DATABASE_URL).crm_db
+import certifi
+
+client = AsyncIOMotorClient(
+    DATABASE_URL,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+
+database: AsyncIOMotorDatabase = client.crm_db
 
 
 async def get_database() -> AsyncIOMotorDatabase:
